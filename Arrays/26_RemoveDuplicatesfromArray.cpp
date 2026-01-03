49677fa64c0d1c8a5ffb7eec4f4337e74bcb01ff
Animesh0508 <97946995+Animesh0508@users.noreply.github.com>
2026-01-04 00:03:00 +0530

    Create 26_RemoveDuplicatesfromArray.cpp

---
+++ Arrays/26_RemoveDuplicatesfromArray.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        
+        int pos = 0;
+        for (int i = 0; i < nums.size(); i++) {
+
+            if (nums[i] != nums[pos]) {
+                nums[pos+1]=nums[i];
+                pos++;
+            }
+        }
+        return pos+1;
+    }
+    
+};
