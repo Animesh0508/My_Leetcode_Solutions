645ff7b48b205f14b2dc396e6f74eb27aaff9de9
Animesh0508 <97946995+Animesh0508@users.noreply.github.com>
2026-01-07 20:38:08 +0530

    Create Intersection_160.cpp

---
+++ LinkedList/Intersection_160.cpp
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        ListNode *currA = headA;ListNode *currB = headB;
+        if (!headA || !headB) return nullptr;
+        while(currA != currB)
+        {
+          currA = (currA==nullptr)?headB:currA->next;
+          currB = (currB==nullptr)?headA:currB->next;
+
+        }
+        return currA;
+    }
+};
